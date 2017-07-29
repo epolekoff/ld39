1be480949f4ed88845c4366ad32a7d9e8c90f447
epolekoff <epolekoff@gmail.com>
2017-07-28 21:16:19 -0700

    Added the game controller and some basic start states.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : Singleton<GameController>, IStateMachineEntity {
+
+    private FiniteStateMachine m_stateMachine;
+    public FiniteStateMachine GetStateMachine(int number = 0)
+    {
+        return m_stateMachine;
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+        m_stateMachine = new FiniteStateMachine(new LevelIntroState(), this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
