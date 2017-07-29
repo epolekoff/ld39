9677359fceba0ef0788c246e81715f9b844b77a9
epolekoff <epolekoff@gmail.com>
2017-07-28 23:39:28 -0700

    Some test sprites. Arm is VERY good.

---
+++ Assets/Scripts/Player/PlayerBlast.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerBlast : MonoBehaviour {
+
+    /// <summary>
+    /// Arm
+    /// </summary>
+    public GameObject ArmObject;
+
+    private const float ChargeRate = 1f;
+    private const float MaxCharge = 3f;
+    private const float ForceChargeMultiplier = 100f;
+
+    private float m_charge = 0f;
+    private bool m_triggerDown = false;
+
+    float m_aimX = 0, m_aimY = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleAiming();
+
+        HandleShooting();
+	}
+
+    /// <summary>
+    /// Aim the direction of the blast.
+    /// </summary>
+    private void HandleAiming()
+    {
+        // Get the angle
+        m_aimX = Input.GetAxis("AimHorizontal");
+        m_aimY = Input.GetAxis("AimVertical");
+
+        // Solve the Look Rotation Is 0 case.
+        if(m_aimX == 0 && m_aimY == 0)
+        {
+            m_aimX = 0.1f;
+        }
+
+        // Rotate by the angle
+        ArmObject.transform.localRotation = Quaternion.LookRotation(new Vector3(m_aimX, -m_aimY, 0), transform.right);
+    }
+
+    private void HandleShooting()
+    {
+        // Hold down the button to charge.
+        if(Input.GetAxis("Fire") > 0 || Input.GetButton("FireButton"))
+        {
+            m_triggerDown = true;
+            m_charge += ChargeRate * Time.deltaTime;
+            if(m_charge > MaxCharge)
+            {
+                m_charge = MaxCharge;
+            }
+        }
+
+        // Release to fire.
+        if ((Input.GetAxis("Fire") == 0 && m_triggerDown) || 
+            Input.GetButtonUp("FireButton"))
+        {
+            m_triggerDown = false;
+
+            // Add the force
+            Vector2 armDirection = new Vector2(m_aimX, -m_aimY);
+            Vector2 force = -armDirection * m_charge * ForceChargeMultiplier;
+            GetComponent<Rigidbody2D>().AddForce(force);
+        }
+    }
+}
