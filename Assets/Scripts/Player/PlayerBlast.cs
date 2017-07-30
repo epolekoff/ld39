5673482558740b4668e2a776aeeb7cbc14b13a7e
epolekoff <epolekoff@gmail.com>
2017-07-29 19:19:42 -0700

    The camera moves in and time slows when using the blast ability.

--- Assets/Scripts/Player/PlayerBlast.cs
+++ Assets/Scripts/Player/PlayerBlast.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Player))]
-public class PlayerBlast : MonoBehaviour {
+public class PlayerBlast : MonoBehaviour
+{
 
     /// <summary>
     /// Arm
@@ -13,25 +14,39 @@ public class PlayerBlast : MonoBehaviour {
     public GameObject Reticle;
     public GameObject ReticleCover;
 
+    private const float MinTimescale = 0.1f;
+    private const float TimescaleDecayRate = 0.5f;
+    private const float MaxSlowdownTime = 3f;
+
+    private const float CameraLerpTime = 5f;
+    private const float CameraResetTime = 0.5f;
+    private float DefaultCameraSize;
+    private Vector3 DefaultCameraPosition;
+    private const float ZoomedCameraSize = 3.26f;
+    private readonly Vector3 ZoomedCameraPosition = new Vector3(0, 0, -10);
+
     private float m_charge = 0f;
     private bool m_triggerDown = false;
     private bool m_charging = false;
 
     float m_aimX = 0, m_aimY = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        DefaultCameraSize = GetComponentInChildren<Camera>().orthographicSize;
+        DefaultCameraPosition = GetComponentInChildren<Camera>().transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         HandleAiming();
 
         HandleShooting();
 
         UpdateReticle();
-	}
+    }
 
     /// <summary>
     /// Aim the direction of the blast.
@@ -43,7 +58,7 @@ public class PlayerBlast : MonoBehaviour {
         m_aimY = Input.GetAxis("AimVertical");
 
         // Solve the Look Rotation Is 0 case.
-        if(m_aimX == 0 && m_aimY == 0)
+        if (m_aimX == 0 && m_aimY == 0)
         {
             m_aimX = 0.1f;
         }
@@ -58,25 +73,26 @@ public class PlayerBlast : MonoBehaviour {
     private void HandleShooting()
     {
         // Hold down the button to charge.
-        if(Input.GetAxis("Fire") > 0 || Input.GetButton("FireButton"))
+        if (Input.GetAxis("Fire") > 0 || Input.GetButton("FireButton"))
         {
             // If you just started charging, trigger some effects
-            if(!m_charging)
+            if (!m_charging)
             {
                 GetComponent<Player>().CharacterSprite.GetComponent<Animator>().SetBool("Charging", true);
             }
 
             m_charging = true;
             m_triggerDown = Input.GetAxis("Fire") > 0;
-            if(m_charge == 0)
+            if (m_charge == 0)
             {
                 m_charge = Designer.Instance.MinCharge;
+                StartCoroutine(BlastSlowEffectCoroutine());
             }
 
             m_charge += Designer.Instance.ChargeRate * Time.deltaTime;
 
             // Cannot exceed the max value.
-            if(m_charge > Designer.Instance.MaxCharge)
+            if (m_charge > Designer.Instance.MaxCharge)
             {
                 m_charge = Designer.Instance.MaxCharge;
             }
@@ -92,7 +108,7 @@ public class PlayerBlast : MonoBehaviour {
         }
 
         // Release to fire.
-        if ((Input.GetAxis("Fire") == 0 && m_triggerDown) || 
+        if ((Input.GetAxis("Fire") == 0 && m_triggerDown) ||
             Input.GetButtonUp("FireButton"))
         {
             m_triggerDown = Input.GetAxis("Fire") == 0;
@@ -128,7 +144,57 @@ public class PlayerBlast : MonoBehaviour {
 
         // Scale the reticle color overlay so it looks like a charge meter thing.
         Vector3 originalScale = ReticleCover.transform.localScale;
-        ReticleCover.transform.localScale = new Vector3((m_charge / Designer.Instance.MaxCharge)/2, originalScale.y, originalScale.z);
+        ReticleCover.transform.localScale = new Vector3((m_charge / Designer.Instance.MaxCharge) / 2, originalScale.y, originalScale.z);
+    }
 
+    /// <summary>
+    /// Slow down timer when charging.
+    /// </summary>
+    private IEnumerator BlastSlowEffectCoroutine()
+    {
+        float timer = 0;
+        Camera camera = GetComponentInChildren<Camera>();
+        Vector3 startPosition = camera.transform.localPosition;
+        float startSize = camera.orthographicSize;
+        while (m_charging && timer < MaxSlowdownTime)
+        {
+            timer += Time.unscaledDeltaTime;
+            if (Time.timeScale > MinTimescale)
+            {
+                Time.timeScale -= TimescaleDecayRate * Time.fixedDeltaTime;
+            }
+
+            Time.fixedDeltaTime = 0.02F * Time.timeScale;
+
+            // Zoom the camera in.
+            camera.transform.localPosition = Vector3.Lerp(startPosition, ZoomedCameraPosition, timer / CameraLerpTime);
+            camera.orthographicSize = Mathf.Lerp(startSize, ZoomedCameraSize, timer / CameraLerpTime);
+
+            yield return null;
+        }
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = 0.02F;
+        StartCoroutine(ResetCameraPositionCoroutine());
+    }
+
+    /// <summary>
+    /// Move the camera back to its position.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ResetCameraPositionCoroutine()
+    {
+        float timer = 0;
+        Camera camera = GetComponentInChildren<Camera>();
+        Vector3 startPosition = camera.transform.localPosition;
+        float startSize = camera.orthographicSize;
+        while (timer < CameraResetTime)
+        {
+            timer += Time.deltaTime;
+
+            camera.transform.localPosition = Vector3.Lerp(startPosition, DefaultCameraPosition, timer / CameraLerpTime);
+            camera.orthographicSize = Mathf.Lerp(startSize, DefaultCameraSize, timer / CameraLerpTime);
+
+            yield return null;
+        }
     }
 }
