80d0bfb3646f4e3509a2748f22848699f18b3944
epolekoff <epolekoff@gmail.com>
2017-07-29 19:30:43 -0700

    Added a menu.

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour {
+
+    public Button PlayButton;
+
+	// Use this for initialization
+	void Start () {
+        EventSystem.current.SetSelectedGameObject(PlayButton.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
